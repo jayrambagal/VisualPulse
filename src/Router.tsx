07680cfa8b6f30ad26d30079cfd13a04bf3d10d6
jaybagall <jayram.bagal@appyhigh.com>
2023-12-24 12:09:53 +0530

    init

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DesignEditor from "~/views/DesignEditor"
+import Dashboard from "~/views/Dashboard"
+
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/manage" element={<Dashboard />} />
+        <Route path="/" element={<DesignEditor />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
