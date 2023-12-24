01cfa99a3b4d1a511aab99ca00444dad3c07846a
JayBagal <jayrambagal00@gmail.com>
2023-12-24 12:41:11 +0530

    init

--- src/constants/app-options.ts
+++ src/constants/app-options.ts
@@ -1,41 +1,41 @@
 export const BASE_ITEMS = [
-  {
-    id: "templates",
-    name: "Templates",
-  },
-  {
-    id: "customize",
-    name: "Customize",
-  },
+  // {
+  //   id: "templates",
+  //   name: "Templates",
+  // },
+  // {
+  //   id: "customize",
+  //   name: "Customize",
+  // },
   {
     id: "elements",
     name: "Elements",
   },
-  {
-    id: "images",
-    name: "Images",
-  },
-  {
-    id: "uploads",
-    name: "Uploads",
-  },
-  {
-    id: "text",
-    name: "Text",
-  },
-  {
-    id: "graphics",
-    name: "Graphics",
-  },
+  // {
+  //   id: "images",
+  //   name: "Images",
+  // },
+  // {
+  //   id: "uploads",
+  //   name: "Uploads",
+  // },
+  // {
+  //   id: "text",
+  //   name: "Text",
+  // },
+  // {
+  //   id: "graphics",
+  //   name: "Graphics",
+  // },
 
-  {
-    id: "pixabay",
-    name: "Pixabay",
-  },
-  {
-    id: "layers",
-    name: "Layers",
-  },
+  // {
+  //   id: "pixabay",
+  //   name: "Pixabay",
+  // },
+  // {
+  //   id: "layers",
+  //   name: "Layers",
+  // },
 ]
 
 export const VIDEO_PANEL_ITEMS = [
